abea3095788ba7897e471c9c0f57bd909279aba7
Dieuthuy211 <diuthuyy1212@gmail.com>
2021-05-17 10:23:56 +0700

    bai 4 module3

---
+++ modulu3/04.common_function/exercise/quan_ly_sinh_vien.sql
@@ -0,0 +1,23 @@
+use quan_ly_sinh_vien;
+
+-- Hiển thị tất cả các thông tin môn học (bảng subject) có credit lớn nhất.
+select id_subject, subject_name,`status`,credit
+from `subject`
+where credit=(select max(credit) from `subject`);
+
+
+
+-- Hiển thị các thông tin môn học có điểm thi lớn nhất.
+ select m.id_subject,m.id_student,max(mark),exam_Times,
+        sb.subject_name
+ from mark m
+ join `subject` sb on sb.id_subject=m.id_subject
+ where mark=(select max(mark) from mark);
+ 
+--  Hiển thị các thông tin sinh viên và điểm trung bình của mỗi sinh viên, xếp hạng theo thứ tự điểm giảm dần
+ select s.id_student,s.student_name,
+        avg(mark) as diem_trung_binh
+from student s
+join mark m on m.id_student=s.id_student
+group by s.id_student
+order by diem_trung_binh desc;
---
+++ modulu3/04.common_function/practice/quan_ly_sinh_vien.sql
@@ -0,0 +1,28 @@
+use quan_ly_sinh_vien;
+
+-- Hiển thị số lượng sinh viên ở từng nơi
+select address,count(address) as so_luong_sinh_vien
+from student
+group by address;
+
+-- Tính điểm trung bình các môn học của mỗi học viên
+select s.id_student,s.student_name,avg(mark) as diem_trung_binh
+from student s
+join mark m on m.id_student=s.id_student
+group by s.id_student;
+
+-- Hiển thị những bạn học viên co điểm trung bình các môn học lớn hơn 15
+select s.id_student,s.student_name,avg(mark) as diem_trung_binh
+from student s
+join mark m on m.id_student=s.id_student
+group by s.id_student
+having diem_trung_binh>15;
+
+
+-- Hiển thị thông tin các học viên có điểm trung bình lớn nhất.
+select s.id_student,s.student_name,avg(mark) as diem_trung_binh
+from student s
+join mark m on m.id_student=s.id_student
+group by s.id_student
+having diem_trung_binh<= all(select avg(mark) from mark m group by m.id_student);
+
